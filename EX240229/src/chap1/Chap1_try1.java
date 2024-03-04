f710ce0fb48f53edb8ba739dff996c747c4c81d8
admin <admin@YD05-09>
2024-03-04 09:44:59 +0900

    2024-03-04

---
+++ EX240229/src/chap1/Chap1_try1.java
@@ -0,0 +1,15 @@
+package chap1;
+
+import java.util.Scanner;
+
+public class Chap1_try1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		System.out.println(a);
+
+	}
+
+}
---
+++ EX240229/src/ex0229/Example.java
@@ -0,0 +1,12 @@
+package ex0229;
+
+public class Example {
+
+	public static void main(String[] args) {
+	
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+	}
+
+}
